cba4e60143f4bfee4871159b5998e84070e10b60
lisav <lisav@DJ-PC-K207-08>
2025-02-18 12:39:18 +0300

    xaml add

--- MasterPolProject/MasterPolProject/Pages/AddEditPage.xaml.cs
+++ MasterPolProject/MasterPolProject/Pages/AddEditPage.xaml.cs
@@ -33,5 +33,46 @@ namespace MasterPolProject.Pages
                 Classes.Manager.MainFrame.GoBack();
             }
         }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder err = new StringBuilder();
+            if (String.IsNullOrEmpty(NameTB.Text))
+            {
+                err.AppendLine("Заполните Наименование!");
+            }
+            if (String.IsNullOrEmpty(TypeComboBox.Text))
+            {
+                err.AppendLine("Заполните Тип партнера!");
+            }
+            if (String.IsNullOrEmpty(RatingTB.Text))
+            {
+                err.AppendLine("Заполните Рейтинг!");
+            }
+            if (String.IsNullOrEmpty(AdressTB.Text))
+            {
+                err.AppendLine("Заполните Адрес!");
+            }
+            if (String.IsNullOrEmpty(DirectorTB.Text))
+            {
+                err.AppendLine("Заполните ФИО Директора!");
+            }
+            if (String.IsNullOrEmpty(PhoneTB.Text))
+            {
+                err.AppendLine("Заполните Номер телефона!");
+            }
+            if (String.IsNullOrEmpty(EmailTB.Text))
+            {
+                err.AppendLine("Заполните Email!");
+            }
+
+            if (err.Length > 0)
+            {
+                MessageBox.Show(err.ToString(), "Внимание!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+
+        }
     }
 }
