8a15271fa2cccdfbc1b11b3a64fec854a45b6c80
Студенты Лисавиной А.В. <lisav@teach.local>
2025-02-21 14:49:06 +0300

    edi

--- MasterPolProject/MasterPolProject/Pages/AddEditPage.xaml.cs
+++ MasterPolProject/MasterPolProject/Pages/AddEditPage.xaml.cs
@@ -23,7 +23,7 @@ namespace MasterPolProject.Pages
 
         public string Flag = "default";
 
-        public Data.Partners _currentProduct = new Data.Partners();
+        public Data.Partners _currentEl = new Data.Partners();
 
         public AddEditPage(Data.Partners product)
         {
@@ -31,24 +31,42 @@ namespace MasterPolProject.Pages
 
             if (product != null)
             {
-                _currentProduct = product;
+                _currentEl = product;
                 Flag = "Edit";
             }
             else
             {
                 Flag = "Add";
             }
-            DataContext = _currentProduct;
+            DataContext = _currentEl;
             
             TypeComboBox.ItemsSource = Data.MasterPolEntities.GetContext().PartnerType.ToList();
+            if(Flag == "Edit")
+            {
             init();
+            }
+            
 
         }
 
         public void init()
         {
+
             TypeComboBox.SelectedItem = Data.MasterPolEntities.GetContext().PartnerType
-                        .Where(d => d.ID == _currentProduct.PartnerTypeID).FirstOrDefault();
+                        .Where(d => d.ID == _currentEl.PartnerTypeID).FirstOrDefault();
+            NameTB.Text = _currentEl.PartnerName;
+            RatingTB.Text = _currentEl.Rating.ToString();
+            AreaTB.Text = _currentEl.Adress.Area.AreaName;
+            CityTB.Text = _currentEl.Adress.City.City1;
+            IndexTB.Text = _currentEl.Adress.Index.IndexNumber;
+            StreetTB.Text = _currentEl.Adress.Street.StreetName;
+            HouseTB.Text = _currentEl.Adress.House.ToString();
+            DirectorTB.Text = _currentEl.Directors.DirectorName;
+            PhoneTB.Text = _currentEl.PartnerPhone.ToString();
+            EmailTB.Text = _currentEl.PartnerEmail.ToString();
+
+
+
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -74,9 +92,25 @@ namespace MasterPolProject.Pages
             {
                 err.AppendLine("Заполните Рейтинг!");
             }
-            if (String.IsNullOrEmpty(AdressTB.Text))
+            if (String.IsNullOrEmpty(AreaTB.Text))
             {
-                err.AppendLine("Заполните Адрес!");
+                err.AppendLine("Заполните Область!");
+            }
+            if (String.IsNullOrEmpty(CityTB.Text))
+            {
+                err.AppendLine("Заполните Город!");
+            }
+            if (String.IsNullOrEmpty(IndexTB.Text))
+            {
+                err.AppendLine("Заполните Индекс!");
+            }
+            if (String.IsNullOrEmpty(StreetTB.Text))
+            {
+                err.AppendLine("Заполните Улица!");
+            }
+            if (String.IsNullOrEmpty(HouseTB.Text))
+            {
+                err.AppendLine("Заполните Дом!");
             }
             if (String.IsNullOrEmpty(DirectorTB.Text))
             {
@@ -98,7 +132,27 @@ namespace MasterPolProject.Pages
 
             else
             {
-                
+                var selectedType = TypeComboBox.SelectedItem as Data.PartnerType;
+
+
+
+
+
+
+
+
+                //    TypeComboBox.SelectedItem = Data.MasterPolEntities.GetContext().PartnerType
+                //        .Where(d => d.ID == _currentEl.PartnerTypeID).FirstOrDefault();
+                //NameTB.Text = _currentEl.PartnerName;
+                //RatingTB.Text = _currentEl.Rating.ToString();
+                //AreaTB.Text = _currentEl.Adress.Area.AreaName;
+                //CityTB.Text = _currentEl.Adress.City.City1;
+                //IndexTB.Text = _currentEl.Adress.Index.IndexNumber;
+                //StreetTB.Text = _currentEl.Adress.Street.StreetName;
+                //HouseTB.Text = _currentEl.Adress.House.ToString();
+                //DirectorTB.Text = _currentEl.Directors.DirectorName;
+                //PhoneTB.Text = _currentEl.PartnerPhone.ToString();
+                //EmailTB.Text = _currentEl.PartnerEmail.ToString();
 
 
             }
